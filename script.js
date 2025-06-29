ec2e32f01b32034285c4b15c5c47b95a96b92e4e
martinramos94 <martinignacioramos@hotmail.com>
2025-06-29 05:04:21 -0300

    finalizacionCarritoDeCompras

--- script.js
+++ script.js
@@ -12,97 +12,54 @@ class Producto{
 	}
 }
 
-let Producto1 = new Producto('arco de tiro', 560000, 45);
-let Producto2 = new Producto('skate', 100200, 28);
-let Producto3 = new Producto('bicicleta', 1200000, 125);
-let Producto4 = new Producto('rollers', 75000, 39);
+let arco = new Producto('arco de tiro', 5, 45);
+let skate = new Producto('skate', 10, 28);
+let bicicleta = new Producto('bicicleta', 12, 125);
+let rollers = new Producto('rollers', 7, 39);
 
 class   CarritoCompras  {
 	
 	items	= [];
 	
-	agregarProducto	(){
-		
-		const	produYcant	=	{};
-		//console.log('producto y cantidad',produYcant);
-		produYcant.producto = Producto3.nombre;
-		produYcant.cantidad = 2;
-		this.items.push(produYcant);	
-		const	produYcant2	=	{};
-		//console.log('producto y cantidad',produYcant2);
-		produYcant2.producto = Producto1.nombre;
-		produYcant2.cantidad = 5;
-		this.items.push(produYcant2);
-		const	produYcant3	=	{};
-		//console.log('producto y cantidad',produYcant2);
-		produYcant3.producto = Producto4.nombre;
-		produYcant3.cantidad = 6;
-		this.items.push(produYcant3);
+	agregarProducto	(producto,cantidad){
+		this.items.push({producto,cantidad});	
 	};
 
 	removerProducto(producto){		
-		this.items = this.items.filter(item => item.producto !== producto )
-		console.log('productos',this.items)
+		this.items = this.items.filter(item => item.producto !== producto );
+		console.log('productos',this.items);
 	}
-	
+
+	calcularTotal(){
+		const subtotal = this.items.reduce((acumulador, cantidad)=>{
+			return acumulador + cantidad.cantidad * cantidad.producto.precio;
+		},0);
+		return subtotal;			
+	}	
+
+	mostrarCarrito(){
+		this.items.forEach((elementos) => {
+			console.log(`producto: ${elementos.producto.nombre}\ncantidad: ${elementos.cantidad}\nSubtotal: $${elementos.producto.precio * elementos.cantidad}`);
+		})
+		console.log(`Total de la compra $${this.calcularTotal()}`)
+	}
 }
 
-const carritoCompras = new CarritoCompras();
-console.log('carrito',carritoCompras);
-carritoCompras.agregarProducto(Producto1);
 
-carritoCompras.removerProducto('rollers');
-//let remover = carritoCompras;
+const carritoCompras = new CarritoCompras();
+carritoCompras.agregarProducto(arco,5);
+carritoCompras.agregarProducto(rollers,7);
+carritoCompras.agregarProducto(bicicleta,2);
+console.log('carrito',carritoCompras);
+
+carritoCompras.removerProducto(rollers);
+
+console.log(`total`,carritoCompras.calcularTotal());
+
+carritoCompras.mostrarCarrito();
 
 
 // console.log(Producto1.mostrar());
 // console.log(Producto2.mostrar());
 // console.log(Producto3.mostrar());
 // console.log(Producto4.mostrar());
-
-
-//CarritoCompras.agregarProducto();
-// carritoCompras.removerProducto();
-// carritoCompras.calcularTotal();
-
-
-
-
-// function	eliminar(){
-	
-// 	let opcion;			
-// 	opcion = prompt(`que producto desea eliminar?\n${producto1.nombre} o ${producto2.nombre}`);
-// 	console.log(opcion);	
-
-// 	for (let cosas of carritoCompras.items){
-		
-// 		console.log('item: ',cosas);
-// 		if(opcion === producto1.nombre){
-// 			delete carritoCompras.items[0];
-// 		}else if(opcion === producto2.nombre) {
-// 			delete carritoCompras.items[1];
-// 		}else{
-// 			carritoCompras.items;
-// 		};
-// 	};
-// 	console.log (carritoCompras);	
-	
-// };
-
-// function	total(){
-
-// 		let selecProduto;
-// 		let precioFinal;
-		
-// 		selecProduto = confirm(`desea comprar ${producto1.nombre} y ${producto2.nombre}?`);
-// 		if (selecProduto===true){
-// 			console.log(selecProduto);
-// 			precioFinal=Number((carritoCompras.items[0].precio)+ carritoCompras.items[1].precio);
-// 			console.log(precioFinal);
-// 		}else {
-			
-// 			alert('los productos se seliminaran del carrito!!');
-// 			delete	carritoCompras.items;
-// 			console.log(carritoCompras.items);
-// 		}
-// };
